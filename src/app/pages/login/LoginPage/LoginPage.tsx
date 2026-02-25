83501f9b22c2ddba8621440ae964b53532dcfcb9
Jac0 <leey153@naver.com>
2026-02-26 08:36:07 +0900

    ✏️ Feat : SignupModal - LoginPage 반영

--- src/app/pages/login/LoginPage/LoginPage.tsx
+++ src/app/pages/login/LoginPage/LoginPage.tsx
@@ -1,5 +1,28 @@
+import { useModal } from "@lib/hooks"
+
+import { SignupModal } from "@app/containers"
+
+import { Button } from "@app/components/Button"
+
 const LoginPage = () => {
-  return <div>로그인 페이지</div>
+  const [isShow, openModal, closeModal] = useModal()
+
+  return (
+    <div>
+      <div>로그인 페이지</div>
+      <Button
+        variant="secondary"
+        size="m"
+        onClick={openModal}>
+        회원가입
+      </Button>
+
+      <SignupModal
+        isShow={isShow}
+        onClose={closeModal}
+      />
+    </div>
+  )
 }
 
 export default LoginPage
