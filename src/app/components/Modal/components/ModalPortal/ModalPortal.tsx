976bfd680a090f4f1237450e249dc8788e01c149
Jac0 <leey153@naver.com>
2026-02-25 12:29:51 +0900

    ✏️ Feat : ModalPortal

---
+++ src/app/components/Modal/components/ModalPortal/ModalPortal.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { createPortal } from "react-dom"
+
+import type { ModalPortalProps } from "./ModalPortal.type"
+
+const ModalPortal = ({ isShow, children }: ModalPortalProps) => {
+  const [modalBaseElement] = useState<HTMLElement | null>(document.body)
+
+  useEffect(() => {
+    if (!modalBaseElement || !isShow) return
+
+    const originalStyle = window.getComputedStyle(modalBaseElement).overflow
+    document.body.style.overflow = "hidden"
+
+    return () => {
+      document.body.style.overflow = originalStyle
+    }
+  }, [isShow, modalBaseElement])
+
+  if (!isShow || !modalBaseElement) {
+    return null
+  }
+
+  return createPortal(<>{children}</>, modalBaseElement)
+}
+
+export default ModalPortal
---
+++ src/app/components/Modal/components/ModalPortal/ModalPortal.type.ts
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react"
+
+export interface ModalPortalProps {
+  children: ReactNode
+  isShow: boolean
+}
