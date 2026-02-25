a2374cab77a0a81251edc464544e985031aadf4d
Jac0 <leey153@naver.com>
2026-02-25 14:42:25 +0900

    ✏️ Feat : useModal Hook

---
+++ src/lib/hooks/useModal/useModal.ts
@@ -0,0 +1,19 @@
+import { useCallback, useState } from "react"
+
+import type { UseModalParams } from "./useModal.type"
+
+const useModal = ({ defaultVisible = false }: UseModalParams = {}) => {
+  const [isShow, setIsShow] = useState(defaultVisible)
+
+  const openModal = useCallback(() => {
+    setIsShow(true)
+  }, [])
+
+  const closeModal = useCallback(() => {
+    setIsShow(false)
+  }, [])
+
+  return [isShow, openModal, closeModal] as const
+}
+
+export default useModal
