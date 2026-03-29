a0f3069c5858d2922ca4666a987fda1fed0e08ba
Jae0 <leey153@naver.com>
2026-03-29 16:54:29 +0900

    ✏️ Feat : useTagsInput

---
+++ src/app/containers/note/NoteContent/components/NoteContentHeader/hooks/useTagInput/index.ts
@@ -0,0 +1,1 @@
+export { default as useTagInput } from "./useTagInput";
---
+++ src/app/containers/note/NoteContent/components/NoteContentHeader/hooks/useTagInput/useTagInput.type.ts
@@ -0,0 +1,8 @@
+export interface TagInputForm {
+  tag: string;
+}
+
+export interface UseTagInputParams {
+  noteNumber: number;
+  tags: string[];
+}
